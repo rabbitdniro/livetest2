2946305d623d58ec2a8ede8f92491b556710dc0d
rabbitdniro <rabbitdniro@gmail.com>
2024-02-27 12:27:35 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {ref} from "vue";
+import ShowObject from './components/ShowObject.vue';
+
+const tasks = ref([
+  {
+    name: "Task 1",
+    time: 30
+  },
+  {
+    name: "Task 2",
+    time: 40
+  },
+  {
+    name: "Task 3",
+    time: 60
+  },
+  {
+    name: "Task 4",
+    time: 45
+  },
+  {
+    name: "Task 5",
+    time: 50
+  },
+
+]);
+
+</script>
+
+<template>
+  <ShowObject v-model="tasks"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ShowObject.vue
@@ -0,0 +1,52 @@
+<script setup>
+  import {ref} from "vue";
+  const props = defineProps(["modelValue"]);
+
+  const userName = ref("");
+  const userTime = ref("");
+  const itemIndex = ref("");
+
+  function shoWPop(id) {
+    itemIndex.value = id;
+    modal.showModal();
+  }
+
+  function closePop() {
+    userName.value = "";
+    userTime.value = "";
+    itemIndex.value = "";
+    modal.close();
+  }
+</script>
+
+<template>
+  <div v-for="(item, index) in modelValue" :key="index">
+    <li class="mb-3" style="list-style-type: none;">
+      {{ item }}
+      <span class="mx-3">
+        <button @click="shoWPop(index)" type="button" class="btn btn-primary">Edit</button>
+      </span>
+    </li>
+  </div>
+
+  <dialog id="modal">
+    <form action="">
+      <label for="name">Name: </label>
+      <input v-model="userName" type="text" name="name" id="name" class="form-control">
+      <br>
+      <label for="time">Time: </label>
+      <input v-model="userTime" type="number" name="time" id="time" class="form-control">
+      <br>
+      <div class="row">
+        <div class="col">
+          <button @click="closePop()" type="button" class="btn btn-outline-primary">Submit</button>
+        </div>
+      </div>
+      
+    </form>
+  </dialog>
+</template>
+
+<style scoped>
+
+</style>
